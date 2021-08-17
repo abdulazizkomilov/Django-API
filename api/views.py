216bef3bf375973d10360b8d108443d76a838fcb
abulaziz <abdulazizkomilov2001@gmail.com>
2021-08-17 13:58:17 +0500

    Initial commit

---
+++ api/views.py
@@ -0,0 +1,7 @@
+from rest_framework.generics import ListAPIView
+from books.models import Book
+from .serializers import BookSerializer
+
+class BookAPIView(ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
